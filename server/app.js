79252dcd726a0e5ef6d2d07a1c04f9527d59595c
JulienBonet <julienbonetpro@gmail.com>
2026-04-04 17:26:04 +0200

    railway deploy fix4

--- server/app.js
+++ server/app.js
@@ -17,14 +17,10 @@ app.use('/images', express.static(path.resolve('public/images')));
 // API
 app.use('/api', router);
 
-// servir React (Vite build)
-const __dirname = new URL('.', import.meta.url).pathname;
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// fallback pour React Router
-app.get('/:any(*)', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+// Fallback React (Express 4)
+app.use(express.static(path.resolve('../client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('../client/dist/index.html'));
 });
 
 export default app;
